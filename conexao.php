5952f9c2e376d195229bb32186fa357c9dac5f42
Edison Protti <38137415+edisonprotti@users.noreply.github.com>
2026-05-21 15:00:47 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,10 +1,10 @@
 <?php
-// Código limpo e sem espaços fantasmas para o Railway
-$servidor = mysql.railway.internal;
-$user = root;
-$pass = qvaBoMQcaojxgfFXUzWRkwZqOTvMxurd;
-$db = railway;
-$port = 3306;
+// Código corrigido com as aspas obrigatórias para strings
+$servidor = 'mysql.railway.internal';
+$user = 'root';
+$pass = 'qvaBoMQcaojxgfFXUzWRkwZqOTvMxurd';
+$db = 'railway';
+$port = '3306'; // Mantenha 3306 se for a porta interna do Railway
 
 try {
     $dsn = "mysql:host={$servidor};port={$port};dbname={$db};charset=utf8mb4";
